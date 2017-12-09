e79e06c6f250cea188d35902e1bb407c387b0805
Andreas Itten <andreas.itten@gmail.com>
2017-12-09 22:24:34 +0100

    introduce SQL

--- sbbdelay3/models.py
+++ sbbdelay3/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Train(models.Model):
+
+    fahrt_bezeichner = models.CharField(max_length=20)
+    bpuic = models.IntegerField()
+    haltestellen_name = models.CharField(max_length=50)
+    linien_text = models.CharField(max_length=20)
+    betriebstag = models.DateTimeField()
+    ankunftszeit = models.DateTimeField()
+    an_prognose = models.DateTimeField()
+    an_prognose_status = models.CharField(max_length=20)
+    abfahrtszeit = models.DateTimeField()
+    ab_prognose = models.DateTimeField()
+    ab_prognose_status = models.CharField(max_length=20)
+    faellt_aus_tf = models.CharField(max_length=20)
+    ab_delay = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
---
+++ sbbdelay3/sbb_data_to_sql.py
@@ -0,0 +1,56 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+import ssl
+from datetime import datetime
+import sqlite3
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+# Load API data
+#https://data.sbb.ch/explore/dataset/actual-data-sbb-previous-day/api/?timezone=Europe%2FZurich&refine.bpuic=8503011
+
+url = "https://data.sbb.ch/api/records/1.0/search/?dataset=actual-data-sbb-previous-day&rows=1000&facet=linien_text&facet=faellt_aus_tf&facet=bpuic&facet=ankunftszeit&facet=an_prognose&facet=an_prognose_status&facet=ab_prognose_status&facet=abfahrtsverspatung&refine.bpuic=8503011&timezone=Europe%2FZurich"
+
+
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read().decode()
+js = json.loads(data)
+print(json.dumps(js["records"], indent=4, sort_keys=True))
+
+
+# Connect to Django SQL DB
+path = "../db.sqlite3"
+#path = "test.db"
+conn = sqlite3.connect(path)
+cur = conn.cursor()
+
+
+# Save data into list and then to SQL
+items = ["fahrt_bezeichner", "bpuic", "haltestellen_name", "linien_text", "betriebstag", "ankunftszeit", "an_prognose", "an_prognose_status", "abfahrtszeit", "ab_prognose", "ab_prognose_status", "faellt_aus_tf"]
+
+for train in js["records"]:
+    info = list()
+    for item in items:
+
+        if item in ["ankunftszeit", "an_prognose", "abfahrtszeit", "ab_prognose"]:
+            train["fields"][item] = datetime.strptime(str(train["fields"][item]), "%Y-%m-%dT%H:%M:%S")
+
+        if item in ["betriebstag"]:
+            train["fields"][item] = datetime.strptime(str(train["fields"][item]), "%Y-%m-%d")
+
+        info.append(train["fields"][item])
+
+    ab_delay = train["fields"]["ab_prognose"] - train["fields"]["abfahrtszeit"]
+    info.append(str(ab_delay))
+
+    #print(info, len(info))
+
+    cur.execute("INSERT OR IGNORE INTO sbbdelay3_train VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?)", info)
+
+    conn.commit()
+
+cur.close()
