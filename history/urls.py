6f088e858ff797f47dafcbbc9b5259c39a082fe6
Andreas Itten <andreas.itten@gmail.com>
2017-12-10 17:36:44 +0100

    Update History

--- history/urls.py
+++ history/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from django.views.generic import ListView, DetailView
-from . import views
-from history.models import Train
+from django.urls import path
+from django.views.generic import ListView
+from history.views import HistoryList
+
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    url(r'^$', ListView.as_view(queryset=Train.objects.all().order_by("ankunftszeit"), template_name="history/index.html")),
+    path("", HistoryList.as_view()),
 ]
--- history/views.py
+++ history/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Train
+from history.est_time import sbbtrip
 
-# def index(request):
-#     query_results = Train.objects.all()
-#     return render(request, "history/index.html", {'query_results': query_results})
+class HistoryList(ListView):
+    template_name = "history/index.html"
+
+    def get_queryset(self):
+        next_trains = sbbtrip()
+        next_train = next_trains["trip1"]["dep"]
+        return Train.objects.order_by("abfahrtszeit").filter(abfahrtszeit__contains=next_train.time())
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context["trips"] = sbbtrip()
+        return context
