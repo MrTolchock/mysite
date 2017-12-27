35fc3a9c82ff8f001007480b9b873a1f47d71e97
Andreas Itten <andreas.itten@gmail.com>
2017-12-27 17:03:43 +0100

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/MrTolchock/mysite/personal/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "personal/static/") #"personal/static"#/home/MrTolchock/mysite/
 
 MEDIA_URL = "/static/media/"
 MEDIA_ROOT = "personal/static/media"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "personal/static/"),
+    #"personal/static/",
 ]
